f0a6b79ab392b3e57419a7c5f2ad78ce4b3979cb
sunlight0o0 <rlawl0423@naver.com>
2022-06-13 09:58:52 +0900

    [test]

---
+++ ks43team03/src/main/java/ks43team03/service/UserService.java
@@ -0,0 +1,30 @@
+package ks43team03.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ks43team03.dto.User;
+import ks43team03.mapper.UserMapper;
+
+/**
+ * @author ksmart
+ * @RequiredArgsConstructor - lombok 의존성 주입
+ */
+@Service
+@Transactional
+public class UserService {
+	
+	private final UserMapper userMapper;
+	
+	public UserService(UserMapper userMapper) {
+		this.userMapper = userMapper;
+	}
+	public List<User> getUserList(){
+		
+		List<User> userList = userMapper.getUserList();
+		
+		return userList;
+	}
+}

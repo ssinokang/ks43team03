a264af9db5ee68213aeaa3eb21ce58092d4d3b98
ksmart43 <seonwonigm@gmail.com>
2022-06-13 17:29:05 +0900

    내꺼

---
+++ ks43team03/src/main/java/ks43team03/controller/LessonController.java
@@ -0,0 +1,44 @@
+package ks43team03.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ks43team03.dto.Lesson;
+import ks43team03.service.LessonService;
+
+@Controller
+@RequestMapping("/lesson")
+public class LessonController {
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+	private final LessonService lessonService;
+	
+	public LessonController(LessonService lessonService) {
+		this.lessonService = lessonService;
+	}
+	
+	@GetMapping("/lessonList")
+	public String lessonList(Model model) {
+		
+		List<Lesson> lessonList = lessonService.getLessonList();
+		model.addAttribute("lessonList", lessonList);
+		return "lesson/lessonList";
+	}
+	@PostMapping("/addLesson")
+	public String addLesson(Lesson lesson) {
+		log.info("lesson : {}", lesson);
+		System.out.println(lesson);
+		return "redirect:/lesson/LessonList";
+		
+	}
+	@GetMapping("/addLesson")
+	public String addLesson() {
+		return "lesson/addLesson";
+	}
+}
---
+++ ks43team03/src/main/java/ks43team03/mapper/LessonMapper.java
@@ -0,0 +1,15 @@
+package ks43team03.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import ks43team03.dto.Lesson;
+
+@Mapper
+public interface LessonMapper {
+
+	//시설 내 레슨 조회하기
+	public List<Lesson> getLessonList();
+	
+}

274bd3b8c9a8d0757e54b9fb9d93be3b8088b6b2
chanbokW <cksqhr4961@naver.com>
2022-06-14 17:33:21 +0900

    FacilityGoods,Pass Java 수정

--- ks43team03/src/main/java/ks43team03/dto/Pass.java
+++ ks43team03/src/main/java/ks43team03/dto/Pass.java
@@ -2,19 +2,21 @@ package ks43team03.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter @Setter
-public class Pass {
+@ToString
+public class Pass extends FacilityGoods{
 	
 	private String passCd;
 	private String facilityGoodsCode;
-	private String facilityCd;
-	private String GoodsCdCtgCd;
-	private String userId;
 	private String passNm;
-	private String passUnit;
-	private String passPrice;
+	private int passUnit;
+	private int passPrice;
 	private String passState;
-	private String passEndDate;
+	private int passEndDate;
+	
+	
+	
 	
 }

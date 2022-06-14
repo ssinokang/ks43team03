f565e62ef039f6f00b451ea528b20d622d1ae33b
jogony <seonwongim@gmail.com>
2022-06-15 08:55:14 +0900

    파일 업로드 업뎃

--- ks43team03/src/main/java/ks43team03/service/LessonService.java
+++ ks43team03/src/main/java/ks43team03/service/LessonService.java
@@ -2,26 +2,27 @@ package ks43team03.service;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
-import ks43team03.controller.UserController;
+import ks43team03.common.FileUtils;
 import ks43team03.dto.Lesson;
+import ks43team03.mapper.FileMapper;
 import ks43team03.mapper.LessonMapper;
 
 @Service
 @Transactional
 public class LessonService {
 	private final LessonMapper lessonMapper;
-		
-	public LessonService(LessonMapper lessonMapper) {
+	private final FileMapper   fileMapper;
+	public LessonService(LessonMapper lessonMapper, FileMapper fileMapper) {
 		this.lessonMapper = lessonMapper;
+		this.fileMapper	  = fileMapper;
 	}
 	
 	// 레슨 리스트 가져오기
@@ -32,24 +33,45 @@ public class LessonService {
 	}
 
 	//레슨 등록하기
-	public void addLesson(Lesson lesson, MultipartHttpServletRequest multipartHttpServletRequest) {
-		if (ObjectUtils.isEmpty(multipartHttpServletRequest) == false) {
-	        Iterator<String> filenameIterator = multipartHttpServletRequest.getFileNames();
+	public void addLesson(Lesson lesson, MultipartHttpServletRequest mhsr) {
+		
+		//파일이 널이 아니라면
+		if (Objects.nonNull(mhsr)) {
+			FileUtils fu = new FileUtils(mhsr, lesson.getUserId());
+			List<Map<String, String>> dtoFileList = fu.parseFileInfo();
+			//t_file 테이블에 삽입
+			fileMapper.uploadFile(dtoFileList);
+			
+			//릴레이션 테이블에 삽입
+			
+			
+			
+			
+			/****************
+			 *				*
+			 *  log 찍어보기	*
+			 * 				*
+			 ****************/
+			
+			/*
+	        Iterator<String> filenameIterator = mhsr.getFileNames();
 	        String name;
 	        System.out.println(filenameIterator.hasNext());
 	        System.out.println(lesson);
 	        while (filenameIterator.hasNext()) {
 	            name = filenameIterator.next();
 	            System.out.println(name + "name!!!!!!!");
-	            List<MultipartFile> fileList = multipartHttpServletRequest.getFiles(name);
+	            List<MultipartFile> fileList = mhsr.getFiles(name);
 	            for (MultipartFile multipartFile : fileList) {
 	                System.out.println("--- start file ---");
 	                System.out.println("File name : " + multipartFile.getOriginalFilename());
 	                System.out.println("File size : " + multipartFile.getSize());
 	                System.out.println("File content-type : " + multipartFile.getContentType());
 	                System.out.println("--- end file ---");
+	                
 	            }
 	        }
+	        */
 	    }
 	}
 }

cdd9e6e3860c0bd9af6ff0c241484cec02b51f31
doitcom <sho0226@gmail.com>
2022-06-15 09:15:25 +0900

    220615 board 수정 ssj

--- ks43team03/src/main/java/ks43team03/dto/Board.java
+++ ks43team03/src/main/java/ks43team03/dto/Board.java
@@ -1,7 +1,5 @@
 package ks43team03.dto;
 
-import java.util.Date;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,11 +11,13 @@ public class Board {
 
 	private String  boardPostCd;
 	private String  boardCtgCd;
+	private String  boardSubCtgCd;
 	private String  userId;
 	private String  boardPostTitle;
 	private String  boardPostContent;
 	private String  boardPostFile;
-	private Date  boardPostDate;
+	private String  boardPostDate;
+	private String  boardPostCommentNum;
 	private int  boardPostViews;
 	private int  boardPostNum;
 	private char  boardPostState;

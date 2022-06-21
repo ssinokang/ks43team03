a027e982a50870c8082540e16078e40c13692b2f
sunlight0o0 <rlawl0423@naver.com>
2022-06-21 09:26:31 +0900

    [북마크 등록]

--- ks43team03/src/main/java/ks43team03/mapper/BoomkMapper.java
+++ ks43team03/src/main/java/ks43team03/mapper/BoomkMapper.java
@@ -11,15 +11,23 @@ import ks43team03.dto.Facility;
 @Mapper
 public interface BoomkMapper {
 	
-	/*
-	 * //즐겨찾기 취소 public int removeBoomkById(String userId);
-	 */
 	
 	//즐겨찾기 조회	
-	public List<Boomk> getBoomkList(String userId);  
+	public List<Boomk> getBoomkList(String userId); 
 	
-	//즐겨찾기 취소
+	//즐겨찾기 등록
+	public int addBoomk(Boomk boomk);
+	
+	
+	//즐겨찾기 삭제
+	public int removeBoomk(Boomk boomk);
+	
+	
+	//즐겨찾기 취소 (N)
 	public int modifyBoomk(Boomk boomk);	
+	
+	//즐겨찾기 추가 (Y)
+	public int modifyBoomk2(Boomk boomk);	
 	
 	//시설 조회
 	public List<Facility> getFacilityList();

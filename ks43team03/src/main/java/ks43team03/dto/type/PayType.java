a54528525bcf68bd9731632e4a1af24f274727af
user <cksqhr4961@naver.com>
2022-06-24 01:36:54 +0900

    결제

--- ks43team03/src/main/java/ks43team03/dto/type/PayType.java
+++ ks43team03/src/main/java/ks43team03/dto/type/PayType.java
@@ -1,5 +1,18 @@
 package ks43team03.dto.type;
 
+import lombok.Getter;
+
+@Getter
 public enum PayType {
-	CARD, KAKAO, NAVER, DEPOSIT
+
+	CARD("카드"),
+	VIRTUAL_ACCOUNT("가상계좌"),
+	MOBILE_PHONE("휴대폰"),
+	TRANSFER("계좌이체");
+	
+	PayType(String name){
+		this.name = name;
+	}
+	
+	private final String name;
 }
---
+++ ks43team03/src/main/java/ks43team03/service/PaymentService.java
@@ -0,0 +1,27 @@
+package ks43team03.service;
+
+public class PaymentService {
+	
+//	토스페이먼츠에서는 다음과 같은 변경 사항에 대해 하위 호환을 지원하는 것으로 판단합니다. 따라서 새로운 버전 릴리즈 없이 기존 API에 반영합니다.
+//
+//	새로운 API 엔드포인트 추가
+//	API 요청에 새로운 선택 파라미터 추가
+//	API 요청에서 사용하는 필수 파라미터를 선택 파라미터로 변경
+//	API 응답에 새로운 필드 추가
+//	새로운 ENUM 추가
+//	에러 메시지 변경
+//	새로운 에러 코드 추가
+//	새로운 웹훅 이벤트 추가
+	
+	private String testClientApiKey;
+	
+	private String testSecretApiKey;
+	
+	private String successCallBackUrl;
+	
+	private String failCallBackUrl;
+	
+	private String tossOriginUrl;
+	
+	
+}

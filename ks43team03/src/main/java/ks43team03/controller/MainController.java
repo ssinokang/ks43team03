35f38b5fab8c6863929854db7cef6e55ecb821ea
ssinokang <kus536@naver.com>
2022-06-24 17:25:46 +0900

    220624

--- ks43team03/src/main/java/ks43team03/controller/MainController.java
+++ ks43team03/src/main/java/ks43team03/controller/MainController.java
@@ -1,10 +1,28 @@
 package ks43team03.controller;
 
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import ks43team03.service.AdminFacilityService;
 
 @Controller
 public class MainController {
+	
+	private final AdminFacilityService adminFacilityService;
+
+	public MainController(AdminFacilityService adminFacilityService) {
+		this.adminFacilityService = adminFacilityService;
+	}
+	
+	
+	private static final Logger log = LoggerFactory.getLogger(MainController.class);
+
 	
 	@GetMapping("/admin")
 	public String adminIndex() {
@@ -22,5 +40,32 @@ public class MainController {
 	@GetMapping("/admin/addGoods")
 	public String addGoods() {
 		return "admin/addGoods";
+	}
+	
+	/**
+	 * 임시 map 컨트롤러
+	 */
+	@GetMapping("/map")
+	public String searchMap() {
+		return "map/map";
+	}
+	
+	@GetMapping("/map2")
+	public String searchMap2(Model model
+			  				,@RequestParam(name = "currentPage", required = false, defaultValue = "1") int currentPage){
+		
+		Map<String, Object> resultMap = adminFacilityService.getAdminFacilityList(currentPage);
+		
+		log.info("resultMap : {}",resultMap);
+		log.info("resultMap.get(\"adminFacilityList\") : {}",resultMap.get("adminFacilityList"));
+		
+		model.addAttribute("resultMap", 			resultMap);
+		model.addAttribute("currentPage", 			currentPage);
+		model.addAttribute("adminFacilityList",		resultMap.get("adminFacilityList"));
+		model.addAttribute("lastPage", 				resultMap.get("lastPage"));
+		model.addAttribute("startPageNum", 			resultMap.get("startPageNum"));
+		model.addAttribute("endPageNum", 			resultMap.get("endPageNum"));
+		model.addAttribute("title", "전체 시설 목록 조회");
+		return "map/map2";
 	}
 }

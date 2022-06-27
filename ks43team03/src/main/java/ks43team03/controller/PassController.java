6ef3d61d4e17883f1d20dc6ac248511ccad78326
chanbokW <cksqhr4961@naver.com>
2022-06-27 14:58:37 +0900

    이용권 crud

--- ks43team03/src/main/java/ks43team03/controller/PassController.java
+++ ks43team03/src/main/java/ks43team03/controller/PassController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -41,21 +42,36 @@ public class PassController {
 
 	@PostMapping("/addPass")
 	public String addPass(Pass pass) {
+		//
+		
+		
 		passService.addPass(pass);
 		return "redirect:/pass/adminPassList";
 	}
 	
 	
-	@GetMapping("/addPass2")
+	@GetMapping("/addPass")
 	public String addPass2(@RequestParam(name = "userId", required = false)String userId) {
 		
+		//권한이 시설관리자인지 아닌지 확인 
 		
-		
-		return "pass/adminAddPass2";
+		return "pass/addPass";
 	}
 	
+	//== 관리자 이용권파트 ==//
+	/**
+	 * 관리자 이용권리스트
+	 * 
+	 */
 	@GetMapping("/admin/passList")
-	public String passList(Model model) {
+	public String adminPassList(Model model) {
+		
+		//권한이 관리자인지 아닌지 체크
+		
+		//페이징 처리
+		
+		
+		//전체이용권리스트
 		List<Pass> passList = passService.getPassAll();
 		model.addAttribute("title", "이용권리스트 관리자");
 		model.addAttribute("passList", passList);
@@ -63,14 +79,122 @@ public class PassController {
 	}
 	
 	
-	
+	/**
+	 * 관리자 이용권상세정보
+	 * 
+	 */
 	@GetMapping("/admin/passDetail")
-	public String passDetail(@RequestParam(name = "passCd")String passCd,@RequestParam(name = "facilityGoodsCd", required = false)String facilityGoodsCd) {
+	public String amdinPassDetail(@RequestParam(name = "passCd")String passCd,
+							 @RequestParam(name = "facilityGoodsCd", required = false)String facilityGoodsCd,
+							 Model model) {
 		log.info("화면에서 받은 facilityGoodsCd 데이터 :  {}", facilityGoodsCd);
 		log.info("화면에서 받은 passCd 데이터 :  {}", passCd);
 		
+		Pass pass = passService.getPassDetail(passCd, facilityGoodsCd);
+		log.info("요청처리의 응답 데이터 : {}", pass);
+		
+		model.addAttribute("title", "이용권상세정보");
+		model.addAttribute("pass", pass);
+		
 		return "pass/passDetail";
 	}
+	
+	
+	
+	
+	//== 시설주 이용권 리스트 ==//
+	/**
+	 * 이용권 리스트 
+	 */
+	@GetMapping("/passList")
+	public String passList(Model model) {
+		
+		//시설 관리자 정보확인  
+		
+		
+		//시설에 있는 관리자가 아니라면
+		
+		
+		//시설에 있는 관리자 정보가 맞다면
+		
+		
+		//페이징 처리
+		String facilityCd = "ss_35011770_03";
+		List<Pass> passList = passService.getPassListOfFacility(facilityCd);
+		model.addAttribute("title", "시설이용권 리스트");
+		model.addAttribute("passList", passList);
+		return "pass/passList";
+	}
+	
+	/**
+	 * 이용권 수정
+	 */
+	@GetMapping("/modifyPass")
+	public String modifyPass(@RequestParam(name = "passCd" ,required = false)String passCd,
+							 @RequestParam(name = "userId" ,required = false)String userId,
+							 @RequestParam(name = "facilityGoodsCd" , required = false)String facilityGoodsCd,
+							 Model model) {
+		
+		
+		//시설 관리자 정보확인  
+
+
+		//시설에 있는 관리자가 아니라면
+		
+		
+		//시설에 있는 관리자 정보가 맞다면
+		log.info("화면에서 받은 passCd데이터 : {}",passCd);
+		log.info("화면에서 받은 userId데이터 : {}",userId);
+		log.info("화면에서 받은 facilityGoodsCd데이터 : {}",facilityGoodsCd);
+		
+		Pass pass = passService.getPassDetail(passCd, facilityGoodsCd);
+		
+		model.addAttribute("title", "시설이용권 수정");
+		model.addAttribute("pass",pass);
+		
+		return "pass/modifyPass";
+	}
+	
+	@PostMapping("/modifyPass")
+	public String modifyPass(Pass pass) {
+		log.info("화면에서 받은 pass data: {}",pass);
+		log.info("화면에서 받은 pass.getPassCd() data: {}",pass.getPassCd());
+		log.info("화면에서 받은 pass.getPassNm() data: {}",pass.getPassNm());
+		passService.modifyPass(pass);
+		return "redirect:/pass/passList";
+	}
+	
+	
+	/**
+	 * 이용권 삭제
+	 */
+	
+	@GetMapping("/removePass")
+	public String removePass(@RequestParam(name = "passCd" ,required = false)String passCd,
+							 @RequestParam(name = "userId" ,required = false)String userId,
+							 @RequestParam(name = "facilityGoodsCd" , required = false)String facilityGoodsCd){
+		
+		//시설 관리자 정보확인  
+		
+		
+		//시설에 있는 관리자가 아니라면
+		
+		
+		//시설에 있는 관리자 정보가 맞다면
+		
+		return "pass/removePass";
+	}
+	
+	
+	
+	@PostMapping("/removePass")
+	public Pass removePass(@RequestBody String passCd) {
+		
+		
+		return new Pass();
+	}
+	
+	
 	
 	
 //	@PostMapping("/addPass2")
--- ks43team03/src/main/java/ks43team03/service/PassService.java
+++ ks43team03/src/main/java/ks43team03/service/PassService.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import ks43team03.dto.FacilityGoods;
 import ks43team03.dto.Pass;
+import ks43team03.exception.NotFoundGoodsException;
 import ks43team03.mapper.FacilityGoodsMapper;
 import ks43team03.mapper.PassMapper;
 
@@ -22,6 +23,13 @@ public class PassService {
 		this.passMapper = passMapper;
 	}
 	
+	/**
+	 * 
+	 * @param pass
+	 * @return 상품코드 pk값
+	 * 이용권 등록 메소드
+	 * 
+	 */
 	public String addPass(Pass pass) {
 		
 		FacilityGoods facilityGoods = pass.toFacilityGoods();
@@ -35,11 +43,69 @@ public class PassService {
 		return facilityGoods.getFacilityGoodsCd();
 	}
 	
-	
+	/**
+	 * 이용권 전체리스트 관리자 페이지 
+	 * @return 이용권 리스트
+	 */
+	@Transactional(readOnly = true)
 	public List<Pass> getPassAll(){
 		return passMapper.getPassAll();
 	}
 	
+	/**
+	 * 
+	 * @param 이용권코드 , 상품코드 
+	 * @return  이용권의 정보
+	 */
+	@Transactional(readOnly = true)
+	public Pass getPassDetail(String passCd, String facilityGoodsCd) {
+		Pass pass = passMapper.getPassDetail(passCd, facilityGoodsCd)
+				.orElseThrow(()->{
+					throw new NotFoundGoodsException("등록하신 이용권이 없습니다.");
+				});
+		
+		return pass;
+	}
+	
+	/**
+	 * 
+	 * @param 시설 코드 
+	 * @return 시설의 이용권리스트
+	 */
+	@Transactional(readOnly = true)
+	public List<Pass> getPassListOfFacility(String facilityCd){
+		
+		List<Pass>passList = passMapper.getPassListOfFacility(facilityCd);
+		return passList;
+	}
+	
+	/**
+	 * 
+	 * @param pass
+	 * 이용권 수정 메소드
+	 */
+	public int modifyPass(Pass pass) {
+		int passResult = passMapper.modifyPass(pass);
+		return passResult;
+	}
+	
+	
+	
+	/**
+	 * 
+	 * @param passCd
+	 * 이용권 삭제 메소드
+	 */
+	public int removePass(String passCd) {
+		return passMapper.removePass(passCd);
+		
+	}
+	
+	
+//	public Pass getPassByPassCd(String passCd) {
+//		Pass pass = passMapper.getPassByPassCd(passCd);
+//		return pass;
+//	}
 	
 	
 	

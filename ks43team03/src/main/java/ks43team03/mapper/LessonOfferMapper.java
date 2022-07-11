bd6d3d61243cc75b3d953c358568f7f68b7ceddf
user <cksqhr4961@naver.com>
2022-07-11 22:16:27 +0900

    LessonOffer 등록 수정삭제 Mapper 추가

--- ks43team03/src/main/java/ks43team03/mapper/LessonOfferMapper.java
+++ ks43team03/src/main/java/ks43team03/mapper/LessonOfferMapper.java
@@ -2,6 +2,7 @@ package ks43team03.mapper;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.apache.ibatis.annotations.Mapper;
 
@@ -10,7 +11,23 @@ import ks43team03.dto.LessonOffer;
 @Mapper
 public interface LessonOfferMapper {
 
+	// LessonOffer List 
 	public List<LessonOffer> getOfferList();
 	
+	// 레슨 offer 지역별 종목별 조회
 	public List<LessonOffer> getLessonOfferCityOrSports(String areaCd,String sportsName);
+	
+	//레슨_offer lesson_offer_cd로조회
+	public Optional<LessonOffer> getLessonOfferByLessonOfferCd(String lessonOfferCd);
+	
+	
+	//lessonOffer 삭제
+	public int removeLessonOffer(String lessonOfferCd);
+	
+	//lessonOffer 수정
+	public int modifyLessonOffer(LessonOffer lessonOffer);
+	
+	//lessonOffer 등록
+	public int addLessonOffer(LessonOffer lessonOffer);
+	
 }
